6f73ae060160f5931d5f37ba21aa7102128ec420
unknown <jorgeabascal20@gmail.com>
2021-06-23 23:01:51 -0500

    Cors para cliente

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 
+app.use( cors() );
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
 // parse application/json
